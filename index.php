1f7a529797d735f3642e6859081c1b855ce8fbe2
Aarcons <62533280+Aarcons@users.noreply.github.com>
2020-10-14 16:40:34 +0530

    design update

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 		  font-family: Raleway;
 		  src: url(fonts/Raleway/Raleway-Regular.ttf);
 		}
-		h1,h2,h3,h4,h5,h6{
+		h1,h2,h3,h4,h5,h6,p,li{
 			font-family: Raleway;
 		}
   		#main-navbar .navbar-brand img{
@@ -28,7 +28,7 @@
   			width: 80%;
   		}
   		#main-navbar{
-  			position: fixed;
+  			position: absolute;
   			z-index: 5
   		}
   		#hero-banner .hero-img{
@@ -154,7 +154,7 @@
 			background-color: #f5f5f5;
 		}
 		#sms-accordion .card-header:focus-within{
-			background-color: #5e2ced;
+			background-color: #2474ae;
 		}
 		#sms-accordion .card-header:focus-within h5{
 			color: #fff!important;
@@ -170,6 +170,59 @@
 		}
 		#message-about .message-about-cont{
 			background-color: #f5fafe;
+		}
+		#tabs .nav-tabs{
+			border: none;
+		}
+		#tabs .nav-tabs .nav-item.show .nav-link, .nav-tabs .nav-link.active{
+			border: none;
+		}
+		#tabs .nav-tabs .nav-link:hover{
+			border: none;
+		}
+		#tabs .nav-item .active{
+			border-bottom: 3px solid #0c6eb5;
+			color: #0c6eb5;
+			font-weight: 700;
+		}
+		#tabs .nav-link{
+			color: #000;
+			font-weight: 500;
+			font-size: 1.1rem;
+			display: grid;
+		}
+		#tabs .icon-container {
+			background-color: #e8f0f9;
+			padding: 1rem 1.1rem;
+			border-radius: 50%;
+			border: 1px solid #2474ae;
+		}
+		#tabs .icon-container i{
+			vertical-align: middle;
+		}
+		#tabs .nav-item .active .icon-container{
+			background-color: #2474ae;
+		}
+		#tabs .nav-item .active .icon-container i{
+			color: #fff;
+		}
+		#tabs .tab-content-container{
+			border-bottom: 1px dashed #2474ae;
+		}
+		#testimonial .carousel-indicators{
+			top: 9rem;
+		}
+		#testimonial .carousel-indicators li{
+			background-color: #000;
+		}
+		#testimonial .carousel-inner i{
+			vertical-align: super;
+		}
+		#testimonial .testimonial-container a i{
+			font-size: 3rem;
+		}
+		#testimonial .testimonial-container a{
+			width: 2%;
 		}
   	</style>
 
@@ -215,12 +268,135 @@
 			</div>
 		</div>
 	</section>
+	<section id="tabs">
+		<div class="container-fluid pt-3 pl-0 pr-0">
+			<h4 class="text-center">We power messages that encourage millions of users to take a next step, everyday. <hr class="divider"></h4>
+			<div class="tab-content-container d-flex justify-content-center mt-5 pl-3 pr-3">
+				<ul class="nav nav-tabs">
+					<li class="nav-item">
+						<a href="#notify" class="nav-link active" data-toggle="tab">
+							<span class="text-center mb-4"><span class="icon-container"><i class="fas fa-bell"></i></span></span>Notify Customers
+						</a>
+					</li>
+					<li class="nav-item">
+						<a href="#confirm-transactions" class="nav-link" data-toggle="tab"><span class="text-center mb-4"><span class="icon-container"><i class="far fa-check-circle"></i></span></span>Confirm Transactions</a>
+					</li>
+					<li class="nav-item">
+						<a href="#drive-sales" class="nav-link" data-toggle="tab"><span class="text-center mb-4"><span class="icon-container"><i class="fas fa-comments-dollar"></i></span></span>Drive Sales & Marketing</a>
+					</li>
+					<li class="nav-item">
+						<a href="#two-factor" class="nav-link" data-toggle="tab"><span class="text-center mb-4"><span class="icon-container"><i class="fas fa-user-lock"></i></span></span>2-Factor Authentication</a>
+					</li>
+					<li class="nav-item">
+						<a href="#fraud-prevention" class="nav-link" data-toggle="tab"><span class="text-center mb-4"><span class="icon-container"><i class="fas fa-fingerprint"></i></span></span>Fraud Prevention</a>
+					</li>
+				</ul>
+			</div>
+			<div class="tab-content mb-5">
+				<div class="container-fluid tab-pane active" id="notify">
+					<div class="row">
+						<div class="col-md-6">
+							<div class="img-col d-flex justify-content-center pt-5">
+								<img src="images/notify2.png" style="height: 100%; width: 100%;">
+							</div>
+						</div>
+						<div class="col-md-6 p-5">
+							<h4 class="text-center mb-3">Notify Customers</h4>
+							<h6 class="mb-4">Integrate our SMS API to send time-critical reminders and notifications.</h6>
+							<ul>
+								<li>Provide timely notifications about pending appointments, arrivals, and more so your customers stay engaged.</li>
+								<li>You can allow a customer to confirm or reschedule an appointment or delivery from a text message - via SMS, MMS or voice.</li>
+								<li>Enhanced multimedia features can be an opportunity to create those "wow" moments that strengthen customer relationships.</li>
+								<li>Reduce missed appointments, delivery attempts, and support costs with automated notifications.</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div class="container-fluid tab-pane" id="confirm-transactions">
+					<div class="row">
+						<div class="col-md-6 p-5">
+							<h4 class="text-center mb-3">Confirm Transactions</h4>
+							<h6 class="mb-4">Send purchase receipts, ticket information, and order confirmations via an SMS message.</h6>
+							<ul>
+								<li>Get notifications either your order placed or not</li>
+								<li>Stay connected with your Respective Bank through our SMS Alert and Request Facility</li>
+								<li>Get your movie ticket and updated news through SMS alert</li>
+								<li>Cancellation and confirmations alert</li>
+							</ul>
+						</div>
+						<div class="col-md-6">
+							<div class="img-col d-flex justify-content-center pt-5">
+								<img src="images/confirm-transaction.png" style="height: 100%; width: 100%;">
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="container-fluid tab-pane" id="drive-sales">
+					<div class="row">
+						<div class="col-md-6">
+							<div class="img-col d-flex justify-content-center pt-5">
+								<img src="images/marketing.png" style="height: 80%; width: 80%;">
+							</div>
+						</div>
+						<div class="col-md-6 p-5">
+							<h4 class="text-center mb-3">Drive Sales and Marketing</h4>
+							<h6 class="mb-4">With high conversion rates, increase sales with SMS marketing campaigns.</h6>
+							<ul>
+								<li>Advertise your products with SMS campaigns to approach your new or existing customers.</li>
+								<li>Increase your sales and drive your business to the next level</li>
+								<li>Grow your business</li>
+								<li>We Provide Awesome Services</li>
+								<li>Exclusive design</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div class="container-fluid tab-pane" id="two-factor">
+					<div class="row">
+						<div class="col-md-6 p-5">
+							<h4 class="text-center mb-3">2-Factor Authentication</h4>
+							<h6 class="mb-4">Time-based one-time passwords and SMS PIN codes for 2FA in a single API.</h6>
+							<ul>
+								<li>One of the most simple and convenient ways to combat on-line fraud.</li>
+								<li>2FA provides a huge amount of protection for important accounts like your email, social media, and bank accounts.</li>
+								<li>2FA puts users in control of how they verify their identity.</li>
+								<li>Your business deserves best software</li>
+							</ul>
+						</div>
+						<div class="col-md-6">
+							<div class="img-col d-flex justify-content-center pt-5">
+								<img src="images/two-factor.png" style="height: 90%; width: 90%;">
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="container-fluid tab-pane" id="fraud-prevention">
+					<div class="row">
+						<div class="col-md-6">
+							<div class="img-col d-flex justify-content-center pt-5">
+								<img src="images/fraud-prevention.png" style="height: 80%; width: 80%;">
+							</div>
+						</div>
+						<div class="col-md-6 p-5">
+							<h4 class="text-center mb-3">Fraud Prevention</h4>
+							<h6 class="mb-4">Industry-standard algorithms, approved IP addresses, & secure HTTPS API connections.</h6>
+							<ul>
+								<li>Trigger SMS, push, and voice notifications from account databases and fraud detection systems using the Azmarq API.</li>
+								<li>Automate SMS and voice notifications such as password resets, low balance alerts, bill reminders, suspicious activity warnings, and other account changes for customers in countries around the world. Handle customer responses using two-way SMS or IVR.</li>
+								<li>Shut down fraud faster and allow your customers to easily manage their account with SMS.</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
 	<section id="message-about">
 		<div class="container-fluid message-about-cont">
 			<div class="row">
-				<div class="col-md-6 pt-3 pb-3">
-					<h3 class="text-center">SMS MARKETING AND ALERTS MADE EASY<hr class="divider"></h3>
-					<div class="accordion" id="sms-accordion">
+				<div class="col-md-6 pt-5 pb-5">
+					<h4 class="text-center ">SMS MARKETING AND ALERTS MADE EASY<hr class="divider"></h4>
+					<div class="accordion pl-4 pr-4" id="sms-accordion">
 					  <div class="card">
 					    <div class="card-header" id="sms-ac-1">
 					      <h2 class="mb-0">
@@ -291,11 +467,63 @@
 					</div>
 				</div>
 				<div class="col-md-6 pt-3 pb-3">
-					<div class="img-div d-flex justify-content-center">
-						<img src="images/group-chat.png" style="height: 100%; width: 100%;">
+					<div class="img-div d-flex justify-content-center pt-5">
+						<img src="images/message3.png" style="height: 65%; width: 65%;">
 					</div>
 				</div>
 			</div>
+		</div>
+	</section>
+	<section id="testimonial">
+		<h4 class="text-center mt-5">What Our Client Say <hr class="divider"></h4>
+		<div class="container mb-5 testimonial-container">
+			<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+			  <div class="carousel-inner mb-5 pl-5 pr-5">
+			    <div class="carousel-item active">
+		            <p class="text-center"><i class="fa fa-quote-left"></i> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		            proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
+		            <i class="fas fa-quote-right"></i></p>
+			    </div>
+			    <div class="carousel-item">
+		            <p class="text-center"><i class="fa fa-quote-left"></i> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		            proident, sunt in culpa qui officia deserunt mollit anim id est laborum. <i class="fas fa-quote-right"></i></p>
+			    </div>
+			    <div class="carousel-item">
+		            <p class="text-center"><i class="fa fa-quote-left"></i> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		            proident, sunt in culpa qui officia deserunt mollit anim id est laborum. <i class="fas fa-quote-right"></i></p>
+			    </div>
+			  </div>
+			  	<ol class="carousel-indicators">
+		          <li data-target="#quote-carousel" data-slide-to="0" class="active">
+		          </li>
+		          <li data-target="#quote-carousel" data-slide-to="1">
+		          </li>
+		          <li data-target="#quote-carousel" data-slide-to="2">
+		          </li>
+		        </ol>
+			  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+			    <span class=""><i class="fas fa-angle-left text-dark"></i></span>
+			    <span class="sr-only">Previous</span>
+			  </a>
+			  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+			    <span class=""><i class="fas fa-angle-right text-dark"></i></span>
+			    <span class="sr-only">Next</span>
+			  </a>
+			</div>
+				
+			
 		</div>
 	</section>
 </body>
